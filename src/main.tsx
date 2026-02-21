549cbd5d95ecb5d0df5a440a803f73e96ddf8b87
Ved <vedpatel6101@gmail.com>
2026-02-21 11:24:48 +0530

    feat: Initialize React application with `StrictMode`, `HashRouter`, and GSAP `ScrollTrigger` setup.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 import gsap from 'gsap';
@@ -10,8 +10,8 @@ gsap.registerPlugin(ScrollTrigger);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/Portfolio">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
